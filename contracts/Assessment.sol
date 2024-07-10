b7dab3fed04e4cb2a23c6729365c54b50f3f164c
adityatiwari-art <130775596+adityatiwari-art@users.noreply.github.com>
2024-07-10 20:33:52 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,72 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
-    uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event OwnerChanged(address indexed oldOwner, address indexed newOwner);
+    event FundsTransferred(address indexed to, uint256 amount);
 
-    constructor(uint initBalance) payable {
+    constructor() payable {
         owner = payable(msg.sender);
-        balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
-    }
-
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
+    modifier onlyOwner() {
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+        _;
     }
 
-    // custom error
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit amount must be greater than zero");
+        emit Deposit(msg.value);
+    }
+
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
+    function withdraw(uint256 _withdrawAmount) public onlyOwner {
+        if (address(this).balance < _withdrawAmount) {
             revert InsufficientBalance({
-                balance: balance,
+                balance: address(this).balance,
                 withdrawAmount: _withdrawAmount
             });
         }
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
+        (bool success, ) = owner.call{value: _withdrawAmount}("");
+        require(success, "Transfer failed");
 
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function showOwner() public view returns (address) {
+        return owner;
+    }
+
+    function changeOwner(address payable newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner address cannot be zero");
+        address oldOwner = owner;
+        owner = newOwner;
+        emit OwnerChanged(oldOwner, newOwner);
+    }
+
+    function transferFunds(address payable to, uint256 amount) public onlyOwner {
+        require(to != address(0), "Cannot transfer to zero address");
+        require(amount > 0, "Transfer amount must be greater than zero");
+        if (address(this).balance < amount) {
+            revert InsufficientBalance({
+                balance: address(this).balance,
+                withdrawAmount: amount
+            });
+        }
+
+        (bool success, ) = to.call{value: amount}("");
+        require(success, "Transfer failed");
+
+        emit FundsTransferred(to, amount);
     }
 }
